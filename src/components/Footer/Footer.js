cd7b2cb230779da73b91bd83adb3def40f7ecaac
VinaySehwag14 <vinaysehwag14@gmail.com>
2022-06-10 19:59:30 +0530

    Final submission

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      target='_blank'
+      rel='noreferrer'
+      href='https://github.com/VinaySehwag14'
+      className='link footer__link'
+    >
+      Vinay Sehwag
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,100 @@
+const header = {
+  // all the properties are optional - can be left empty or deleted
+  homepage: '/',
+  title: 'Vinay',
+}
+
+const about = {
+  // all the properties are optional - can be left empty or deleted
+  name: 'Vinay Sehwag',
+  role: 'FullStack Developer',
+  description:
+    'Hi there, A little about myself i am currently doing my bacherlors in computer science and have worked as a freelancer.',
+  resume:
+    'https://drive.google.com/drive/folders/1-3RKK2OLHwjzHid-Nd_XYeLyyksn7_kr?usp=sharing',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/vinay-sehwag-malang08/',
+    github: 'https://github.com/VinaySehwag14',
+    twitter: 'https://twitter.com/VinaySehwag14',
+  },
+}
+
+const projects = [
+  // projects can be added an removed
+  // if there are no projects, Projects section won't show up
+  {
+    name: 'Kisaan Ki dukaan ',
+    description: 'An E-commerce platform for farmers using MERN stack',
+    stack: ['MONGODB', 'Express', 'React', 'Node.js'],
+    sourceCode: 'https://github.com/VinaySehwag14/E-comm-Frontend',
+
+    livePreview: 'https://kisaan-mern.herokuapp.com/',
+  },
+  {
+    name: 'Netflix Clone',
+    description: 'A netflix clone web app made by using react and firebase',
+    stack: ['CSS', 'Firebase', 'React'],
+    sourceCode: 'https://github.com/VinaySehwag14/NetflixClone-React',
+    livePreview: 'https://netflix-clone-668b8.web.app/',
+  },
+  {
+    name: 'IBlog',
+    description:
+      'A blogging web application made by using React , nodejs , mongodb and express',
+    stack: ['MongoDB', 'React', 'Node.js', 'Express'],
+    sourceCode: 'https://github.com/VinaySehwag14/iBlog-frontend',
+    livePreview: 'https://ibloog-frontend.herokuapp.com/',
+  },
+  {
+    name: 'Nextjs blog with MARKDOWN',
+    description:
+      'A simple Nextjs Personal Blog using MARKDOWN where I can share code also ',
+    stack: ['Reactjs', 'Nextjs', 'Tailwind', 'Api'],
+    sourceCode: 'https://github.com/VinaySehwag14/Nextjs-Blog-App',
+    livePreview: 'https://nextjs-blog-app-eight-lyart.vercel.app/',
+  },
+  {
+    name: 'Smart Home Mobile App',
+    description:
+      'A Smart Home app that controls you home electronic devices through nodemcu',
+    stack: ['Flutter', 'C++', 'Node.js'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Smart Home Mobile App',
+    description:
+      'A Smart Home app that controls you home electronic devices through nodemcu',
+    stack: ['Flutter', 'C++', 'Node.js'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+]
+
+const skills = [
+  // skills can be added or removed
+  // if there are no skills, Skills section won't show up
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'NextJs',
+  'Node.js',
+  'MongoDB',
+  'Firebase',
+  'Python',
+  'SASS',
+  'Tailwind',
+  'Git',
+  'Figma',
+  'Adobe xd',
+  'UI/Ux',
+]
+
+const contact = {
+  // email is optional - if left empty Contact section won't show up
+  email: 'vinaysehwag14@gmail.com',
+}
+
+export { header, about, projects, skills, contact }
